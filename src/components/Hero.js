4d5b771f52a5af8bf8306f8a8cb24b6a73ff47c1
daincoding <official.toyou@gmail.com>
2025-03-18 18:02:31 +0100

    Setting up Dev env. and Entry

---
+++ src/components/Hero.js
@@ -0,0 +1,8 @@
+export default function Hero() {
+    return (
+      <section className="h-screen flex flex-col items-center justify-center bg-gray-800 text-white">
+        <h1 className="text-4xl font-bold mb-20">Welcome to My Portfolio</h1>
+        <p className="mt-2 text-lg">I build amazing web experiences</p>
+      </section>
+    );
+  }
