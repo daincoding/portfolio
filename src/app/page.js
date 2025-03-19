0bfaa6278ca4589b8798da8160907fc900cff352
daincoding <official.toyou@gmail.com>
2025-03-19 16:24:43 +0100

    About.js Main Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 
@@ -7,6 +8,7 @@ export default function Home() {
       <Sidebar />
       <div className="main-content">
         <Navbar />
+        <About />
       </div>
     </div>
   );
