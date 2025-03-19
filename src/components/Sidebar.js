b894d4277cbc287d9e12b83119cb068473894b8b
daincoding <official.toyou@gmail.com>
2025-03-19 15:53:25 +0100

    Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,85 +1,105 @@
-import Image from "next/image"
-import dain from "../../public/assets/DainAvatar.jpg"
-import { FaChevronDown } from "react-icons/fa"
-import { CiMail } from "react-icons/ci"
+"use client";
+
+import { FaChevronDown, FaGithub } from "react-icons/fa";
+import { FaBluesky, FaDiscord } from "react-icons/fa6";
+import { CiMail, CiLinkedin, CiLocationOn } from "react-icons/ci";
+import { VscGithubAlt } from "react-icons/vsc";
+import { useState } from "react";
 
 const Sidebar = () => {
-  return (
-    <aside>
-        <div>
-            <figure>
-                <Image src={dain} alt="Avatar"/>
-            </figure>
-            <div>
-                <h1>Alexander Panske</h1>
-                <p>Web Developer</p>
-            </div>
-            <button>
-                <span>Show Contacts</span>
-                    <FaChevronDown />
-            </button>
-        </div>
-        <div>
-            <div></div>
-            <ul>
-                <li>
-                    <div>
-                        <CiMail />
-                    </div>
-                    <div>
-                        <p>Email</p>
-                        <a href="mailto:alexander.panske@yahoo.com">alexander.panske@yahoo.com</a>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <CiMail />
-                    </div>
-                    <div>
-                        <p>Phone</p>
-                        <a href="mailto:alexander.panske@yahoo.com">alexander.panske@yahoo.com</a>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <CiMail />
-                    </div>
-                    <div>
-                        <p>Birthday</p>
-                        <a href="mailto:alexander.panske@yahoo.com">alexander.panske@yahoo.com</a>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <CiMail />
-                    </div>
-                    <div>
-                        <p>Location</p>
-                        <a href="mailto:alexander.panske@yahoo.com">alexander.panske@yahoo.com</a>
-                    </div>
-                </li>
-            </ul>
-            <div></div>
-            <ul>
-                <li>
-                    <a href="#">
-                        <CiMail />
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <CiMail />
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <CiMail />
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </aside>
-  )
-}
+  
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-export default Sidebar
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+    console.log("Sidebar state:", !isSidebarOpen);
+  };
+
+  return (
+    <aside className={`sidebar ${isSidebarOpen ? "active" : ""}`} data-sidebar>
+      <div className="sidebar-info">
+        <figure className="avatar-box">
+          <img src="/assets/DainAvatar.jpg" alt="Richard Hanrick" width={80} className="avatar-box"/>
+        </figure>
+
+        <div className="info-content">
+          <h1 className="name">Alexander Panske</h1>
+          <p className="title">Web Developer</p>
+        </div>
+
+        <button onClick={toggleSidebar} className="info_more-btn" data-sidebar-btn>
+          <span>Show Contacts</span>
+          <FaChevronDown />
+        </button>
+      </div>
+
+      {/* Sidebar Details - Always Exists, But Shown/Hidden */}
+      <div className={`sidebar-info-more ${isSidebarOpen ? "active" : ""}`}>
+        <div className="separator"></div>
+        <ul className="contacts-list">
+          <li className="contact-item">
+            <div className="icon-box">
+                <CiMail className="icon"/>
+            </div>
+            <div className="contact-info">
+              <p className="contact-title">Email</p>
+              <a href="mailto:richard@example.com" className="contact-link">alexander.panske@yahoo.com</a>
+            </div>
+          </li>
+          <li className="contact-item">
+            <div className="icon-box">
+                <VscGithubAlt className="icon"/>
+            </div>
+            <div className="contact-info">
+              <p className="contact-title">GitHub</p>
+              <a href="https://github.com/daincoding" className="contact-link">daincoding</a>
+            </div>
+          </li>
+          <li className="contact-item">
+            <div className="icon-box">
+                <CiLinkedin className="icon"/>
+            </div>
+            <div className="contact-info">
+              <p className="contact-title">LinkedIn</p>
+              <a href="https://www.linkedin.com/in/alexpanske/" className="contact-link">Alexander Panske</a>
+            </div>
+          </li>
+          <li className="contact-item">
+          <div className="icon-box">
+                <CiLocationOn className="icon"/>
+            </div>
+            <div className="contact-info">
+              <p className="contact-title">Location</p>
+              <address>Hanover, Lower Saxony, Germany 🇩🇪</address>
+            </div>
+          </li>
+        </ul>
+        <div className="separator"></div>
+
+        <ul className="social-list">
+
+          <li className="social-item">
+            <a href="https://github.com/daincoding" className="social-link">
+              <FaGithub name="logo-github"></FaGithub>
+            </a>
+          </li>
+
+          <li className="social-item">
+            <a href="https://bsky.app/profile/dainsounds.nohit.club" className="social-link">
+            <FaBluesky name="logo-bluesky"></FaBluesky>
+            </a>
+          </li>
+
+          <li className="social-item">
+            <a href="https://discordapp.com/users/246708539182022656" className="social-link">
+            <FaDiscord name="logo-discord"></FaDiscord>
+            </a>
+          </li>
+
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
