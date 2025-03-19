10a986d797230674ea8dcb861942b834da4ab584
daincoding <official.toyou@gmail.com>
2025-03-19 10:02:53 +0100

    CSS Ground Setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
-import About from "@/components/About";
-import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Hero />
-      <About />
+    <div>  
     </div>
   );
 }
